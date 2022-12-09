a237a06ca200bdfb029f0ff97fce1d8ad32431ed
Victoria Pfeiffer <killerskiss.xp11@gmail.com>
2022-12-09 11:31:21 -0500

    Dashboard Update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,13 +2,11 @@
 export const environment = {
   production: false,
   firebase: {
-    projectId: 'kanbanfire-3975b',
-    appId: '1:917319811802:web:e43d84b846973647ec5be3',
-    storageBucket: 'kanbanfire-3975b.appspot.com',
-    locationId: 'us-central',
-    apiKey: 'AIzaSyASYZGbVuz9RKGWJl3TQc-Gn3l7IQ5Y8JA',
-    authDomain: 'kanbanfire-3975b.firebaseapp.com',
-    messagingSenderId: '917319811802',
-    measurementId: 'G-6SD3ED80FR',
+    apiKey: "AIzaSyBCmTgZuA0e3-J3DdgAxj9TLvM-ZMYTfdk",
+    authDomain: "angular-work-order.firebaseapp.com",
+    projectId: "angular-work-order",
+    storageBucket: "angular-work-order.appspot.com",
+    messagingSenderId: "623370459015",
+    appId: "1:623370459015:web:70631fb18506f313aba522"
   }
 };
